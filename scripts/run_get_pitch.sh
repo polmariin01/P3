70d98b150b7db9f59a2ffcaa06030a23031c0720
polmariin01 <pol.marin.gargallo@gmail.com>
2022-04-19 04:24:51 +0200

    samba bahia

--- scripts/run_get_pitch.sh
+++ scripts/run_get_pitch.sh
@@ -1,26 +1,24 @@
 #!/bin/bash
-if [ $# -ne 1 ]; then
-
-    f="1"
+if [ $# -ne 2 ]; then
+    um="0.45"
+    u1="0.95"
     #echo "Usage: $0 alpha1 alpha2"
     #exit -1
 else
-#    ex=$1
-#    um=$2
+    um=$1
+    u1=$2
 #    u1=$3
 #    pot=$4
-    f=$1
 fi
-ds="4"
-um="0.45"
-u1="0.95"
+#u1="0.95"
+ds="5"
 pot="-20"
 ex="0"
 # Put here the program (maybe with path)
 #GETF0="get_pitch --umaxnorm=$um --u1norm=$u1 --upot=$pot --uext=$ex"
 #GETF0="get_pitch --umaxnorm=$um --upot=$pot --uext=$ex"
 #GETF0="get_pitch --cclipping --umaxnorm=$um --uext=$ex"
-GETF0="get_pitch --cclipping=0.009 --umaxnorm=$um --u1norm=$u1 --upot=$pot --uext=$ex --downsampl=$ds --lpfwindow=$f"
+GETF0="get_pitch --cclipping=0.009 --umaxnorm=$um --u1norm=$u1 --upot=$pot --uext=$ex --downsampl=$ds --lpfwindow=1"
 
 for fwav in pitch_db/train/*.wav; do
     ff0=${fwav/.wav/.f0}
